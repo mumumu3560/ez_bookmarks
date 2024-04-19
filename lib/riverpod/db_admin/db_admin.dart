5f8a39b133058dc992e95ab7b793e86d1b0f22fe
mumumu3560 <puzmgamgam121@gmail.com>
2024-04-19 10:21:38 +0900

    ここでdbを管理してみる。
    db_switcherの方で管理しようとも思ったが試してみる。
    
    variousのmyDatabaseを変更してこちらにできるか？

---
+++ lib/riverpod/db_admin/db_admin.dart
@@ -0,0 +1,28 @@
+
+import 'package:ez_bookmarks/drift/database_1/database.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'db_admin.g.dart';
+
+@riverpod
+class DbAdminNotifier extends _$DbAdminNotifier{
+
+  @override
+  MyDatabase build() {
+    //最初はez_databaseを返す。
+    //もしくはshared_preferencesから取得する。
+    String db = "ez_database";
+    MyDatabase myDB = MyDatabase(dbName: db);
+    return myDB;
+  }
+
+  //ここでdb変更。
+  //ez_database、ez_database_1、ez_database_2のいずれかを引数に取る。
+  Future<void> updateState(String db) async{
+    //myDatabaseをいったん破棄して、新しいdbを作成する。
+    await state.close();
+    state = MyDatabase(dbName: db);
+  }
+
+
+}
---
+++ lib/riverpod/db_admin/db_admin.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'db_admin.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dbAdminNotifierHash() => r'b6a2d2babe8cfae26f143c68d052ff26ff9337c7';
+
+/// See also [DbAdminNotifier].
+@ProviderFor(DbAdminNotifier)
+final dbAdminNotifierProvider =
+    AutoDisposeNotifierProvider<DbAdminNotifier, MyDatabase>.internal(
+  DbAdminNotifier.new,
+  name: r'dbAdminNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$dbAdminNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DbAdminNotifier = AutoDisposeNotifier<MyDatabase>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
