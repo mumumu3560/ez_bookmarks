aea7743c4ca72558ed736cdf34b7b42c77a38ce0
mumumu3560 <puzmgamgam121@gmail.com>
2024-04-20 07:02:45 +0900

    hooks+consumerでinterstitial広告を流したい。

--- lib/pages/setting/setting_page.dart
+++ lib/pages/setting/setting_page.dart
@@ -1,17 +1,22 @@
 
 import 'dart:io';
 import 'package:ez_bookmarks/admob/inline_adaptive_banner.dart';
+import 'package:ez_bookmarks/env/env.dart';
 import 'package:ez_bookmarks/riverpod/db_admin/db_admin.dart';
 import 'package:ez_bookmarks/riverpod/db_switcher/db_switcher.dart';
 import 'package:ez_bookmarks/riverpod/theme/theme_switcher.dart';
 import 'package:ez_bookmarks/utils/various.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 //ここは設定ページでプライバシーポリシー、テーマ切り替え、データベースの切り替えなどを行える
 
-class SettingPage extends ConsumerWidget{
+class SettingPage extends HookConsumerWidget{
   const SettingPage({super.key});
 
   static Map<int,String> dbMap = {
@@ -20,18 +25,47 @@ class SettingPage extends ConsumerWidget{
     2: "ez_database_2",
   };
 
+  static Map<String, String> dbNameMap = {
+    "ez_database": "DB1",
+    "ez_database_1": "DB2",
+    "ez_database_2": "DB3",
+  };
+
   static Map<String, int> dbReMap = {
     "ez_database": 0,
     "ez_database_1": 1,
     "ez_database_2": 2,
   };
-  
-  
+
+
+  static final String adUnitId = Env.i1;
 
 
   @override
   Widget build(BuildContext context, WidgetRef ref){
 
+    final interstitialAd = useState<InterstitialAd?>(null);
+    final isAdLoaded = useState(false);
+
+    useEffect(() {
+      InterstitialAd.load(
+        adUnitId: adUnitId,
+        request: const AdRequest(),
+        adLoadCallback: InterstitialAdLoadCallback(
+          onAdLoaded: (ad) {
+            interstitialAd.value = ad;
+            isAdLoaded.value = true;
+          },
+          onAdFailedToLoad: (error) {
+            print('広告のロードに失敗しました: $error');
+          },
+        ),
+      );
+
+      return () {
+        interstitialAd.value?.dispose();
+      };
+    }, const []);
 
     final nowTheme = ref.watch(themeModeSwitcherNotifierProvider);
 
@@ -39,6 +73,7 @@ class SettingPage extends ConsumerWidget{
     //final myDatabase = ref.watch(dbAdminNotifierProvider.notifier);
 
     final dbAdmin = ref.watch(dbAdminNotifierProvider);
+
 
     return Scaffold(
       appBar: AppBar(
@@ -56,26 +91,7 @@ class SettingPage extends ConsumerWidget{
                   title: const Text('プライバシーポリシー'),
                   onTap: () async{
             
-                    if(Platform.isAndroid){
-                        const String url = "https://ez-bookmarks.pages.dev/ja/privacy-policy";
-                        if(await canLaunchUrl(Uri.parse(url))){
-                          await launchUrl(Uri.parse(url));
-                        }
-            
-                      }
-            
-                      if(Platform.isIOS){
-            
-                      }
-            
-                      const String url = "https://ez-bookmarks.pages.dev/ja/privacy-policy";
-            
-                      if(Platform.isWindows){
-                        if(await canLaunchUrl(Uri.parse(url))){
-                          await launchUrl(Uri.parse(url));
-                        }
-            
-                      }
+                    
             
                   }
                 ),
@@ -86,14 +102,10 @@ class SettingPage extends ConsumerWidget{
                       nowTheme.when(
                         data: (data) => IconButton(
                           onPressed: () async{
-                            //final themeNotifier = ref.read(themeModeSwitcherNotifierProvider.notifier);
-                            //themeNotifier.updateState(data == 0 ? 1 : 0);
-                            //await themeNotifier.updateState(data == 0 ? 1 : 0);
 
                             int? thisTheme = await dbAdmin.getTheme();
                             final themeNotifier = ref.read(themeModeSwitcherNotifierProvider.notifier);
-                            print("げんざいのてーああああああああああああああああああああああああ: $thisTheme");
-                    
+                            
                             if (thisTheme == 0) {
                               await themeNotifier.updateState(1);
                               //myDatabase.updateTheme(1);
@@ -128,7 +140,7 @@ class SettingPage extends ConsumerWidget{
             
                     int? thisTheme = await dbAdmin.getTheme();
                     final themeNotifier = ref.read(themeModeSwitcherNotifierProvider.notifier);
-                    print("げんざいのてーああああああああああああああああああああああああ: $thisTheme");
+                    
             
                     if (thisTheme == 0) {
                       themeNotifier.updateState(1);
@@ -158,44 +170,53 @@ class SettingPage extends ConsumerWidget{
             
                 ListTile(
                   title: const Text('データベースの切り替え'),
-                  subtitle: Column(
+                  subtitle: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    //幅を指定
+
                     children: dbMap.entries.map((entry) {
                       return ElevatedButton(
                         style: ButtonStyle(
                           backgroundColor: MaterialStateProperty.resolveWith<Color>(
                             (states) {
                               // 現在選択されているデータベース名と一致する場合は異なる色を設定
-                              return dbName == entry.value ? Colors.blue : Colors.grey;
+                              return dbName == entry.value ? Colors.blue : Colors.transparent;
                             },
                           ),
                         ),
                         onPressed: () async{
+
+                          if (isAdLoaded.value) {
+                            interstitialAd.value!.show();
+                            interstitialAd.value!.fullScreenContentCallback = FullScreenContentCallback(
+                              onAdDismissedFullScreenContent: (InterstitialAd ad) {
+                                ad.dispose();
+                                isAdLoaded.value = false;
+                                // 広告が閉じられた後にデータベース切り替え処理を実行
+                              },
+                            );
+                          }
+
                           final notifier = ref.read(dbSwitcherNotifierProvider.notifier);
                           notifier.updateState(entry.value);
 
                           final dbAdNotifier = ref.read(dbAdminNotifierProvider.notifier);
                           await dbAdNotifier.closeDB();
                           await dbAdNotifier.updateDB(entry.value);
-                          print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-                          print("こんにちはここでDBを変更します。");
-                          print("現在のデータベース: $dbName");
-                          print("変更後のデータベース: ${entry.value}");
-
-                          print(dbAdNotifier);
 
                         },
-                        child: Text(entry.value),
+                        child: Text(dbNameMap[entry.value]!),
                       );
                     }).toList(),
                   ),
                 ),
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                      padding: EdgeInsets.all(16),
-                      child: Text('現在のデータベース: $dbName'),
-                    ),
+
+
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    child: Text('現在のデータベース: ${dbNameMap[dbName]}'),
                   ),
                 ),
 
