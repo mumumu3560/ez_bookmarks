41450b09924176bef6f3521b4f0b91658cd59abd
mumumu3560 <puzmgamgam121@gmail.com>
2024-04-19 16:28:30 +0900

    現在のDBを参照してインポートとバックアップを行える

--- lib/pages/database_management/components/almost_logic/almost_logic.dart
+++ lib/pages/database_management/components/almost_logic/almost_logic.dart
@@ -1,17 +1,22 @@
 import 'dart:io';
+import 'package:ez_bookmarks/riverpod/db_admin/db_admin.dart';
+import 'package:ez_bookmarks/riverpod/db_switcher/db_switcher.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:ez_bookmarks/drift/database_1/database.dart'; // 正しいパスに修正してください
 
 import 'package:ez_bookmarks/utils/various.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:path/path.dart' as p;
 
 import 'package:external_path/external_path.dart';
 import 'dart:typed_data';
 
+import 'package:path_provider/path_provider.dart';
 
-Future<void> backupDatabase(BuildContext context, String dbName) async {
+
+Future<void> backupDatabase(WidgetRef ref, BuildContext context, String dbName) async {
     String? path;
 
     if(Platform.isWindows){
@@ -63,8 +68,12 @@ Future<void> backupDatabase(BuildContext context, String dbName) async {
 
       }
 
+      final dbAd = ref.watch(dbAdminNotifierProvider);
+
       // データベースのバックアップを新しいファイルに作成
-      await myDatabase.backupDatabase(path);
+      //await myDatabase.backupDatabase(path);
+
+      await dbAd.backupDatabase(path);
 
 
       if(context.mounted){
@@ -76,13 +85,32 @@ Future<void> backupDatabase(BuildContext context, String dbName) async {
 
   // データベースのインポート
   //dbNameで指定されたデータベースにインポートする
-  Future<void> importDatabase(BuildContext context,  String dbName) async {
+  Future<void> importDatabase(WidgetRef ref,  BuildContext context,  String dbName) async {
     final FilePickerResult? result = await FilePicker.platform.pickFiles();
     if (result != null && result.files.single.path != null) {
       final String path = result.files.single.path!;
-      await myDatabase.importDatabase(path, dbName);
+      //await myDatabase.importDatabase(path, dbName);
 
-      myDatabase = MyDatabase(dbName: dbName); // データベースを再作成
+      //final dbAd = ref.read(dbAdminNotifierProvider);
+      final dbAdNotifier = ref.read(dbAdminNotifierProvider.notifier);
+
+      final dbName = ref.read(dbSwitcherNotifierProvider);
+
+      final nowDbName = dbName;
+
+
+
+      //await myDatabase.close(); // 最初にデータベース接続を閉じる
+      await dbAdNotifier.closeDB();
+
+      final dbFolder = await getApplicationDocumentsDirectory();
+      final file = File(p.join(dbFolder.path, '$dbName.sqlite'));
+
+      await file.delete();
+      await File(path).copy(file.path);
+
+      //myDatabase = MyDatabase(dbName: dbName); // データベースを再作成
+      await dbAdNotifier.updateDB(nowDbName);
 
       if(context.mounted){
         context.showSuccessSnackBar(message: 'データベースがインポートされました');
@@ -90,3 +118,23 @@ Future<void> backupDatabase(BuildContext context, String dbName) async {
       // 必要に応じてアプリケーションの再起動やデータベースの再読み込みする
     }
   }
+
+
+
+
+
+
+  /*
+  
+  // 新しいデータベースファイルで現在のデータベースを置き換える
+  Future<void> importDatabase(String newPath, String dbName) async {
+
+    await myDatabase.close(); // 最初にデータベース接続を閉じる
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, '$dbName.sqlite'));
+
+    await file.delete();
+    await File(newPath).copy(file.path);
+    // データベースの再接続や初期化が必要な場合はここで実行
+  }
+   */
