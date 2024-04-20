48e156e90d2176289c4ac8edb687142ed318732d
mumumu3560 <puzmgamgam121@gmail.com>
2024-04-20 13:44:54 +0900

    インタースティシャルの制御。 BookmarkListなどでプリロードがしたかったのでriverpodにした。
    BookmarkListのinitStateでloadを行う。
    
    またデータベース切り替え時に、3回に一回でinterstitial広告が流れるようにする。
    interstitialCountの方で回数は管理する。

---
+++ lib/riverpod/interstitial/interstitial_ad_notifier.dart
@@ -0,0 +1,70 @@
+import 'package:ez_bookmarks/env/env.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'interstitial_ad_notifier.g.dart'; // 自動生成ファイル
+
+@riverpod
+class InterstitialAdNotifier extends _$InterstitialAdNotifier {
+
+  final adUnitId = Env.i1;
+  @override
+  InterstitialAd? build() {
+
+    return null;
+  }
+
+  void loadAd() {
+    if (state == null) { // 広告がまだロードされていない場合
+      InterstitialAd.load(
+        adUnitId: adUnitId,
+        request: const AdRequest(),
+
+        adLoadCallback: InterstitialAdLoadCallback(
+
+          onAdLoaded: (InterstitialAd ad) {
+            state = ad;
+            _setCallbacks(ad); // コールバックを設定
+          },
+
+          onAdFailedToLoad: (LoadAdError error) {
+            
+            state = null; // エラーが発生したため、stateをnullに設定
+          },
+
+        ),
+      );
+    }
+  }
+
+  Future<void> showAd() async {
+    if(state == null) return; // 広告がロードされていない場合は何もしない
+    state!.show();
+  }
+
+  void _setCallbacks(InterstitialAd ad) {
+    ad.fullScreenContentCallback = FullScreenContentCallback(
+      onAdDismissedFullScreenContent: (InterstitialAd ad) {
+
+        ad.dispose();
+        state = null; // 広告を表示後にnullに設定
+        loadAd(); // 広告を再ロードする（オプション）
+        
+
+      },
+      onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
+
+        ad.dispose();
+        state = null; // 広告を表示後にnullに設定
+
+      },
+    );
+  }
+
+  void disposeAd() {
+    if(state != null) {
+      state!.dispose();
+      state = null;
+    }
+  }
+}
---
+++ lib/riverpod/interstitial/interstitial_ad_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'interstitial_ad_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$interstitialAdNotifierHash() =>
+    r'd1b769d8fc1a7c976df6feece0837e00374015b8';
+
+/// See also [InterstitialAdNotifier].
+@ProviderFor(InterstitialAdNotifier)
+final interstitialAdNotifierProvider = AutoDisposeNotifierProvider<
+    InterstitialAdNotifier, InterstitialAd?>.internal(
+  InterstitialAdNotifier.new,
+  name: r'interstitialAdNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$interstitialAdNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$InterstitialAdNotifier = AutoDisposeNotifier<InterstitialAd?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
