25bdc3659a5b7d867455f8b3f59f19ee04a4dcb9
mumumu3560 <puzmgamgam121@gmail.com>
2024-04-24 16:58:59 +0900

    ネイティブ広告周り

--- android/app/src/main/kotlin/com/divcurious/ez_bookmarks/MainActivity.kt
+++ android/app/src/main/kotlin/com/divcurious/ez_bookmarks/MainActivity.kt
@@ -1,5 +1,53 @@
+/*
+package com.divcurious.ez_bookmarks
+
+import io.flutter.embedding.android.FlutterActivity
+class MainActivity: FlutterActivity()
+
+
+ */
+
+/*
+
+
+ */
+
+
 package com.divcurious.ez_bookmarks
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+// COMPLETE: Import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngine
+
+// COMPLETE: Import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
+import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        GoogleMobileAdsPlugin.registerNativeAdFactory(
+                flutterEngine, "listTile", ListTileNativeAdFactory(context))
+    }
+
+    override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
+        super.cleanUpFlutterEngine(flutterEngine)
+
+        GoogleMobileAdsPlugin.unregisterNativeAdFactory(flutterEngine, "listTile")
+    }
+}
+
+
+
+
+
+
+/*
+
+
+ */
+
+
+
+
