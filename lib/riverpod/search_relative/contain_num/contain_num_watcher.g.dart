080f3796a0db58ad12dea36a8f5b979e5a5035b1
mumumu3560 <puzmgamgam121@gmail.com>
2024-04-24 16:54:36 +0900

    前回のコミットと同様に、サーチページで使おうとしたriverpod

---
+++ lib/riverpod/search_relative/contain_num/contain_num_watcher.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'contain_num_watcher.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$containNumWatcherNotifierHash() =>
+    r'28956b59679ed914623ba67323638845d6b17976';
+
+/// See also [ContainNumWatcherNotifier].
+@ProviderFor(ContainNumWatcherNotifier)
+final containNumWatcherNotifierProvider =
+    AutoDisposeNotifierProvider<ContainNumWatcherNotifier, int>.internal(
+  ContainNumWatcherNotifier.new,
+  name: r'containNumWatcherNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$containNumWatcherNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ContainNumWatcherNotifier = AutoDisposeNotifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
