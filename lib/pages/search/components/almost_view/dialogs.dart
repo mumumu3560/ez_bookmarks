08fd8324ef4fb1139fd2b742a142e33c3f584d94
mumumu3560 <puzmgamgam121@gmail.com>
2024-04-24 16:56:17 +0900

    ヘルプの変更

--- lib/pages/search/components/almost_view/dialogs.dart
+++ lib/pages/search/components/almost_view/dialogs.dart
@@ -5,66 +5,46 @@ import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 const List<String> searchHelpContents = [
   'このページではタグからブックマークを検索したり、タグのジャンルを変更することができます。',
-
-
   'ブックマークに登録されたタグ一覧を下から選んで検索できます。\n\nタグは複数選択できます。',
-
-
   'ジャンルはタグを分類したもので、デフォルトは「分類なし」となっています。\n\nタグを選択し、ジャンルを変更することができます。',
 ];
+
 void showSearchHelpDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        final PageController pageController = PageController(initialPage: 0);
-        final List<Widget> pages = [
-          HelpPage(content: searchHelpContents[0], title: "ページについて"),
-          HelpPage(content: searchHelpContents[1], title: "検索について",),
-          HelpPage(content: searchHelpContents[2], title: "ジャンルについて"),
-          
-        ];
-        return Dialog(
-          child: Container(
-            height: SizeConfig.blockSizeVertical! * 50,  // 高さを適切に設定
-            width: SizeConfig.blockSizeHorizontal! * 80,  // 幅を適切に設定
-            child: Column(
-              children: [
-              
-                Expanded(
-                  child: PageView(
-                    controller: pageController,
-                    children: pages,
-                  ),
-                ),
-                SizedBox(height: 10,),
-                SmoothPageIndicator(
+  showDialog(
+    context: context,
+    builder: (context) {
+      final PageController pageController = PageController(initialPage: 0);
+      final List<Widget> pages = [
+        HelpPage(content: searchHelpContents[0], title: "ページについて"),
+        HelpPage(content: searchHelpContents[1], title: "検索について"),
+        HelpPage(content: searchHelpContents[2], title: "ジャンルについて"),
+      ];
+      return Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(0.0), // 角を丸くする
+        ),
+        child: Container(
+          height: SizeConfig.blockSizeVertical! * 50,  // 高さを適切に設定
+          width: SizeConfig.blockSizeHorizontal! * 80,  // 幅を適切に設定
+          child: Column(
+            children: [
+              Expanded(
+                child: PageView(
                   controller: pageController,
-                  count: pages.length,
-                  effect: WormEffect(),
+                  children: pages,
                 ),
-
-                SizedBox(height: 10,)
-              ],
-            ),
+              ),
+              SizedBox(height: 10,),
+              SmoothPageIndicator(
+                controller: pageController,
+                count: pages.length,
+                effect: WormEffect(),
+              ),
+              SizedBox(height: 10,)
+            ],
           ),
-        );
-      },
-    );
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+        ),
+      );
+    },
+  );
+}
