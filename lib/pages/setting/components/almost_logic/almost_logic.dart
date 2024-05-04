7afc4894f5d66ac4e96f8a855f76966944d3a437
mumumu3560 <puzmgamgam121@gmail.com>
2024-05-05 02:02:34 +0900

    GDPRの変更のロジック追加

--- lib/pages/setting/components/almost_logic/almost_logic.dart
+++ lib/pages/setting/components/almost_logic/almost_logic.dart
@@ -1,6 +1,7 @@
 import 'package:ez_bookmarks/riverpod/db_admin/db_admin.dart';
 import 'package:ez_bookmarks/riverpod/db_switcher/db_switcher.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 Future<void> performDatabaseSwitch(String dbValue, WidgetRef ref) async {
   final notifier = ref.read(dbSwitcherNotifierProvider.notifier);
@@ -9,4 +10,20 @@ Future<void> performDatabaseSwitch(String dbValue, WidgetRef ref) async {
   final dbAdNotifier = ref.read(dbAdminNotifierProvider.notifier);
   await dbAdNotifier.closeDB();
   await dbAdNotifier.updateDB(dbValue);
+}
+
+
+//GDPRの同意メッセージの表示
+Future<void> changePrivacyPreferences() async{
+  ConsentInformation.instance
+      .requestConsentInfoUpdate(ConsentRequestParameters(), () async {
+    if (await ConsentInformation.instance.isConsentFormAvailable()) {
+      ConsentForm.loadConsentForm((consentForm) {
+        consentForm.show((formError) async {
+        });
+      }, (formError) {
+      });
+    }
+  }, (error) {
+  });
 }
